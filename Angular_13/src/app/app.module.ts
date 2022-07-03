42f9049906fb4aac9b2671914aba9679136c097b
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-07-02 23:14:28 -0300

    criando_o_primeiro_'component'_do_projeto

--- Angular_13/src/app/app.module.ts
+++ Angular_13/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent
   ],
   imports: [
     BrowserModule,
