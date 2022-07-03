68cd7ff787e0cd510515f004fa902b5c25ce1369
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-07-03 00:25:38 -0300

    colocando_dados_da_classe_do_componente_no_template_este_curso_e_conhecido_como_interpolacao

--- Angular_13/src/app/components/first-component/first-component.component.ts
+++ Angular_13/src/app/components/first-component/first-component.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Luis Henrique S. F';     // criando uma propriedade
+  age: number = 30;
+  formation = 'Análise e desenvolvimento de sistemas';
+  courses = ["Técnico em informatica para internet", " Técnico de eletroeletronica", " Programador Front End", " Lógica de programação"];
 
   constructor() { }
 
