57eaa0e434d4f06b368cc9169f56170fe33eff21
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-07-03 23:27:30 -0300

    criando_uma_renderizacao_condiconal_ou_seja_imprimindo_algo_no_template_baseado_em_uma_condicional_'if'_e_'else'

--- Angular_13/src/app/app.module.ts
+++ Angular_13/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { DirectivesComponent } from './componets/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     ParentDataComponent,
-    DirectivesComponent
+    DirectivesComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular_13/src/app/components/if-render/if-render.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-if-render',
+  templateUrl: './if-render.component.html',
+  styleUrls: ['./if-render.component.css']
+})
+export class IfRenderComponent implements OnInit {
+  canShow: boolean = true;
+  name = 'Luis henrique s f';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
