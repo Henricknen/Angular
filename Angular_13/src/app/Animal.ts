49aea562dd1103f267175c9a7f969b8c7bd977ad
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-07-05 19:41:16 -0300

    ultilizando_interface_para_representar_entidades_no_sistema_eventos_ultiliza_este_recurso

---
+++ Angular_13/src/app/Animal.ts
@@ -0,0 +1,5 @@
+export interface Animal {       // criação da interface
+  name: string    // propriedades
+  type: string
+  age: number
+}
--- Angular_13/src/app/components/list-render/list-render.component.ts
+++ Angular_13/src/app/components/list-render/list-render.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent implements OnInit {
-  animals = [   // lista de animais "propriedade"
-    { name: 'Fred', type: 'Cachorro'},
-    { name: 'kiara', type: 'Gato'},
-    { name: 'Luke', type: 'Cachorro'},
-    { name: 'Bob', type: 'Cavalo'},
+  animals: Animal[] = [   // tipando como um array "lista" de animais
+    { name: 'Fred', type: 'Cachorro', age: 7},
+    { name: 'kiara', type: 'Gato', age: 3},       // lista de animais "propriedade"
+    { name: 'Luke', type: 'Cachorro', age: 10},
+    { name: 'Bob', type: 'Cavalo', age: 1},
   ];
+
+  animal: Animal = {
+    name: 'Tete',
+    type: 'Alguma coisa',
+    age: 10,
+  }
+
+  animalDetails = ''
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  showAge (animal: Animal) {        // metodo que recebe um animal
+    this.animalDetails = `O animal ${animal.name} tem ${animal.age} anos`;
   }
 
 }
