aa11031d11bbae5effc262c9bcc59dd73cca54d9
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-07-16 23:53:45 -0300

    criando_validacoes_no_formulario_com_recurso_chamado_'reactive_forms'

--- momentos/src/app/components/momento-form/momento-form.component.ts
+++ momentos/src/app/components/momento-form/momento-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-momento-form',
@@ -7,9 +8,35 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MomentoFormComponent implements OnInit {
   @Input() btnText!: string;
+
+  momentoform!: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.momentoform = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('',[Validators.required]),
+      descripition: new FormControl('',[Validators.required]),
+      imagem: new FormControl('')
+    })
+  }
+
+  get title() {
+    return this.momentoform.get('title')!;
+  }
+
+  get descripition() {
+    return this.momentoform.get('descripition')!;
+  }
+
+  submit() {
+
+    if(this.momentoform.invalid) {
+      return;
+    }
+
+    console.log("Enviou formulario!!!");
   }
 
 }
