ba3c05e8155900d7aa62acf17ab8582bce48e0e8
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-07-24 01:09:25 -0300

    resgatando_dados_do_banco_de_dados_via_'api'_assim_preenchendo_as_views

--- momentos/src/app/components/pages/home/home.component.ts
+++ momentos/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,11 @@
+import { MomentoService } from './../../../services/momento.service';
+import { environment } from './../../../../environments/environment';
 import { Component, OnInit } from '@angular/core';
+
+
+import { Momento } from 'src/app/momento';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +14,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  allMomentos: Momento[] = []     /* este array pega os 'momentos' do banco de dados */
+  momentos: Momento[] = []    /* vi ser a exibição depois da busca */
+  baseApiUrl = environment.baseApiUrl   /* busca dados da 'api' */
+
+  // todo search
+
+  constructor(private momentoService: MomentoService) { }
 
   ngOnInit(): void {
+    this.momentoService.getMomentos().subscribe((items) => {    /* iniçialização dos momentos do 'search' */
+
+      const data = items.data
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+          );
+      })
+
+      this.allMomentos = data;
+      this.momentos = data;
+
+    })
   }
 
 }
--- momentos/src/app/services/momento.service.ts
+++ momentos/src/app/services/momento.service.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Momento } from '../momento';
+
+import { Resposta } from '../resposta';
+
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -13,6 +16,10 @@ export class MomentoService {
   private apiUrl = `${this.baseApiUrl}api/momentos`
 
   constructor(private http: HttpClient) { }
+
+  getMomentos(): Observable<Resposta<Momento[]>> {    /* metodo responssavel por trazer todos os momentos */
+    return this.http.get<Resposta<Momento[]>>(this.apiUrl);
+}
 
   createMomento(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
